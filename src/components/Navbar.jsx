d8096de40cceacad8814ea3d0a5f65d3a56b4f56
Saron <saronabebe2019@gmail.com>
2026-09-25 13:18:23 -0800

    Make the navigation section in mobile devices a dropdown menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,63 +1,127 @@
-
+import { useState } from "react";
 import { NavLink } from "react-router";
+import MenuIcon from "@mui/icons-material/Menu";
+import CloseIcon from "@mui/icons-material/Close";
 import { useCartStore } from "../store/cartStore.js";
 
 function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
 
   const cartItems = useCartStore((state) => state.cartItems);
-const itemCount = cartItems.reduce((sum, item) => sum + item.quantity, 0);
+  const itemCount = cartItems.reduce((sum, item) => sum + item.quantity, 0);
 
+  // Mobile-first Approach
   return (
-    <nav className="flex flex-col sm:flex-row sm:justify-between sm:items-center px-6 sm:px-10 py-4 bg-white/95 backdrop-blur-md shadow-md border-b border-gray-100 gap-4">
-      <div className="text-2xl font-extrabold tracking-tight text-blue-600">
-        Prodexa
+    <>
+      <nav className="flex justify-between items-center px-10 py-4 bg-white/95 backdrop-blur-md shadow-md border-b border-gray-100 gap-4 md:px-6 ">
+        <div className="text-2xl font-extrabold tracking-tight text-blue-600">
+          Prodexa
+        </div>
+
+        <div className="hidden md:flex flex-wrap items-center gap-1 sm:gap-3">
+          <NavLink
+            to="/"
+            className="px-4 py-2 rounded-lg text-sm font-medium text-gray-600 hover:text-blue-600 hover:bg-blue-50 transition-all duration-200"
+          >
+            Home
+          </NavLink>
+
+          <NavLink
+            to="/products"
+            className="px-4 py-2 rounded-lg text-sm font-medium text-gray-600 hover:text-blue-600 hover:bg-blue-50 transition-all duration-200"
+          >
+            Products
+          </NavLink>
+
+          <NavLink
+            to="/about"
+            className="px-4 py-2 rounded-lg text-sm font-medium text-gray-600 hover:text-blue-600 hover:bg-blue-50 transition-all duration-200"
+          >
+            About
+          </NavLink>
+
+          <NavLink
+            to="/contact"
+            className="px-4 py-2 rounded-lg text-sm font-medium text-gray-600 hover:text-blue-600 hover:bg-blue-50 transition-all duration-200"
+          >
+            Contact
+          </NavLink>
+
+          <NavLink to="/cart" className="text-gray-700 hover:text-blue-600">
+            Cart {itemCount > 0 && `(${itemCount})`}
+          </NavLink>
+
+          <NavLink
+            to="/login"
+            className="px-5 py-2 rounded-lg text-sm font-semibold text-white bg-blue-600 hover:bg-blue-700 shadow-sm hover:shadow-md transition-all duration-200"
+          >
+            Login
+          </NavLink>
+        </div>
+        {/* Mobile Hamburger button - visible on mobile and hidden for larger screens */}
+        <div className="flex md:hidden">
+          <button
+            type="button"
+            className="text-gray-400 hover:text-white focus:outline-none"
+            aria-label="Toggle menu"
+            onClick={() => setIsOpen(!isOpen)}
+          >
+            {/* Dynamic content - if not opened => hamburger manu, if opened => close button */}
+            {isOpen ? <CloseIcon /> : <MenuIcon />}
+          </button>
+        </div>
+      </nav>
+
+      {/* MOBILE MENU DROPDOWN: Only renders/displays when isOpen is true, and automatically hidden on desktop */}
+      <div
+        className={`${isOpen ? "block" : "hidden"} md:hidden bg-slate-800 border-t border-slate-700 w-80 self-center`}
+      >
+        <div className="px-2 pt-2 pb-4 space-y-1 sm:px-3 flex flex-col">
+          <NavLink
+            to="/"
+            className="px-4 py-2 rounded-lg text-sm font-medium text-white hover:text-blue-600 hover:bg-blue-50 transition-all duration-200"
+          >
+            Home
+          </NavLink>
+
+          <NavLink
+            to="/products"
+            className="px-4 py-2 rounded-lg text-sm font-medium text-white hover:text-blue-600 hover:bg-blue-50 transition-all duration-200"
+          >
+            Products
+          </NavLink>
+
+          <NavLink
+            to="/about"
+            className="px-4 py-2 rounded-lg text-sm font-medium text-white hover:text-blue-600 hover:bg-blue-50 transition-all duration-200"
+          >
+            About
+          </NavLink>
+
+          <NavLink
+            to="/contact"
+            className="px-4 py-2 rounded-lg text-sm font-medium text-white hover:text-blue-600 hover:bg-blue-50 transition-all duration-200"
+          >
+            Contact
+          </NavLink>
+
+          <NavLink
+            to="/cart"
+            className="px-4 py-2 rounded-lg text-sm font-medium text-white hover:text-blue-600 hover:bg-blue-50 transition-all duration-200 px-4"
+          >
+            Cart {itemCount > 0 && `(${itemCount})`}
+          </NavLink>
+
+          <NavLink
+            to="/login"
+            className="px-4 py-2 rounded-lg text-sm font-medium text-white hover:text-blue-600 hover:bg-blue-50 transition-all duration-200"
+          >
+            Login
+          </NavLink>
+        </div>
       </div>
-
-      <div className="flex flex-wrap items-center gap-1 sm:gap-3">
-        <NavLink
-          to="/"
-          className="px-4 py-2 rounded-lg text-sm font-medium text-gray-600 hover:text-blue-600 hover:bg-blue-50 transition-all duration-200"
-        >
-          Home
-        </NavLink>
-
-        <NavLink
-          to="/products"
-          className="px-4 py-2 rounded-lg text-sm font-medium text-gray-600 hover:text-blue-600 hover:bg-blue-50 transition-all duration-200"
-        >
-          Products
-        </NavLink>
-
-        <NavLink
-          to="/about"
-          className="px-4 py-2 rounded-lg text-sm font-medium text-gray-600 hover:text-blue-600 hover:bg-blue-50 transition-all duration-200"
-        >
-          About
-        </NavLink>
-
-        <NavLink
-          to="/contact"
-          className="px-4 py-2 rounded-lg text-sm font-medium text-gray-600 hover:text-blue-600 hover:bg-blue-50 transition-all duration-200"
-        >
-          Contact
-        </NavLink>
-
-        <NavLink to="/cart" className="text-gray-700 hover:text-blue-600">
-  Cart {itemCount > 0 && `(${itemCount})`}
-</NavLink>
-      
-        <NavLink
-          to="/login"
-          className="px-5 py-2 rounded-lg text-sm font-semibold text-white bg-blue-600 hover:bg-blue-700 shadow-sm hover:shadow-md transition-all duration-200"
-        >
-          Login
-        </NavLink>
-
-        
-      </div>
-    </nav>
+    </>
   );
 }
 
 export default Navbar;
-
